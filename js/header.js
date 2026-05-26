238b417e96ddbf2f28d44c2b2cc12d7e602cd146
candyclick <qgqgqhqyus@gmail.com>
2026-05-27 01:48:19 +0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -6,7 +6,7 @@ function renderHeader() {
   header.innerHTML = `
     <nav class="navbar" role="navigation" aria-label="Main navigation">
       <div class="nav-container">
-        <a href="index.html" class="nav-logo" aria-label="Candy Clicker Home">
+        <a href="/" class="nav-logo" aria-label="Candy Clicker Home">
           <span class="logo-icon">🍭</span>
           <span class="logo-text">Candy<span class="logo-accent">Clicker</span></span>
         </a>
@@ -14,14 +14,14 @@ function renderHeader() {
           <span></span><span></span><span></span>
         </button>
         <ul class="nav-links" id="navLinks" role="menubar">
-          <li role="none"><a href="#game" role="menuitem" class="nav-link">Play Now</a></li>
-          <li role="none"><a href="#candies" role="menuitem" class="nav-link">Candy Types</a></li>
-          <li role="none"><a href="#upgrades" role="menuitem" class="nav-link">Upgrades</a></li>
-          <li role="none"><a href="#leaderboard" role="menuitem" class="nav-link">Leaderboard</a></li>
-          <li role="none"><a href="#how-to-play" role="menuitem" class="nav-link">How to Play</a></li>
-          <li role="none"><a href="#features" role="menuitem" class="nav-link">Features</a></li>
+          <li role="none"><a href="/#game" role="menuitem" class="nav-link">Play Now</a></li>
+          <li role="none"><a href="/#candies" role="menuitem" class="nav-link">Candy Types</a></li>
+          <li role="none"><a href="/#upgrades" role="menuitem" class="nav-link">Upgrades</a></li>
+          <li role="none"><a href="/#leaderboard" role="menuitem" class="nav-link">Leaderboard</a></li>
+          <li role="none"><a href="/#how-to-play" role="menuitem" class="nav-link">How to Play</a></li>
+          <li role="none"><a href="/#features" role="menuitem" class="nav-link">Features</a></li>
         </ul>
-        <a href="#game" class="nav-cta">Start Clicking!</a>
+        <a href="/#game" class="nav-cta">Start Clicking!</a>
       </div>
     </nav>
   `;
